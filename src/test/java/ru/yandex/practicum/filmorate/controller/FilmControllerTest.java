3f9443f6f6e603956d75dcc76628750ca19b5e9c
andrewbak37 <91086922+andrewbak37@users.noreply.github.com>
2022-07-17 15:28:58 +0300

    add-friends-likes

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.validation.FilmValidation;
 
 import java.time.LocalDate;
@@ -12,13 +13,13 @@ class FilmControllerTest {
 
     @Test
     void validationTest() {
-        final FilmController filmController = new FilmController();
-        final Film film = new FilmValidation();
+        final Film film = new Film();
+        final FilmValidation filmValidation = new FilmValidation();
         film.setName("name");
         film.setDescription("description");
         film.setReleaseDate(LocalDate.of(2000, 1, 1));
         film.setDuration(113);
-        filmController.filmValidation.filmsValidation(film);
+        filmValidation.filmsValidation(film);
         assertEquals(film.getName(), "name");
         assertEquals(film.getDescription(), "description");
         assertEquals(film.getReleaseDate(), LocalDate.of(2000, 1, 1));
